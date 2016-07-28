46d09329471d1e027fcbab975effb705bd4fbb6e
gggcgba@163.com <gggcgba777830400>
2016-07-28 08:13:53 +0800

    添加工具类

---
+++ src/main/java/com/wsp/tao/springmvc/entity/SysPermission.java
@@ -0,0 +1,70 @@
+package com.wsp.tao.springmvc.entity;
+
+import javax.persistence.*;
+import java.util.Date;
+
+/**
+ * Created by wangshupeng1 on 2016/7/28.
+ * 按钮权限
+ */
+
+@Entity
+@Table(name = "t_sys_permission", schema = "", catalog = "tao")
+public class SysPermission {
+    private int id;
+    private String code;
+    private String description;
+    private Date createTime;
+
+    private SysResource resource;	//所属菜单
+
+    @Id
+    @Column(name = "id")
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "code")
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+    @Basic
+    @Column(name = "description")
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Basic
+    @Column(name = "create_time")
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    @ManyToOne(cascade=CascadeType.REFRESH,fetch = FetchType.EAGER)
+    @JoinColumn(name = "resource_id",nullable = false)
+    public SysResource getResource() {
+        return resource;
+    }
+
+    public void setResource(SysResource resource) {
+        this.resource = resource;
+    }
+
+}
---
+++ src/main/java/com/wsp/tao/springmvc/entity/SysResource.java
@@ -0,0 +1,101 @@
+package com.wsp.tao.springmvc.entity;
+
+import javax.persistence.*;
+import java.util.Date;
+
+/**
+ * Created by wangshupeng1 on 2016/7/28.
+ */
+@Entity
+@Table(name = "t_sys_resource", schema = "", catalog = "tao")
+public class SysResource {
+    private int id;
+    private String name;
+    private String code;
+    private String url;
+    private int type;
+    private int parentId;
+    private String description;
+    private Date createTime;
+
+    @Id
+    @Column(name = "id")
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "name")
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Basic
+    @Column(name = "code")
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    @Basic
+    @Column(name = "url")
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    @Basic
+    @Column(name = "type")
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    @Basic
+    @Column(name = "parent_id")
+    public int getParentId() {
+        return parentId;
+    }
+
+    public void setParentId(int parentId) {
+        this.parentId = parentId;
+    }
+
+    @Basic
+    @Column(name = "description")
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Basic
+    @Column(name = "create_time")
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+}
